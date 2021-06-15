06322da04c8d0ad2ebc75055c8e19bb882d3334a
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-06-16 00:01:23 +0530

    Added s3 bucket

--- credible/settings/base.py
+++ credible/settings/base.py
@@ -15,6 +15,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
@@ -37,7 +39,8 @@ INSTALLED_APPS = [
     'dashboard',
     'personality',
     'reviews',
-    'twitter'
+    'twitter',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -114,8 +117,23 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_ACCESS_KEY_ID = 'AKIAZIJW2VQP5V4ODKPI'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'credible-bucket-1'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_DEFAULT_ACL = 'public-read-write'
+
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+]
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
