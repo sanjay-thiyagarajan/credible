d6ff5a4e5f084c9e0b4014fcde966892ac7b9f5c
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-06-16 21:39:04 +0530

    Fixed few bugs

--- twitter/utils/twitter_sentiment_analysis.py
+++ twitter/utils/twitter_sentiment_analysis.py
@@ -8,7 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import plotly.graph_objects as go
-from textblob import TextBlob
 from expertai.nlapi.cloud.client import ExpertAiClient
 from plotly.io import to_html
 
@@ -62,48 +61,35 @@ class TweetAnalyzer():
 
     def clean_tweet(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
-
-    
-    def analyze_sentiment(self, tweet):
-        analysis = TextBlob(self.clean_tweet(tweet))
-        if analysis.sentiment.polarity > 0:
-            return 1
-        elif analysis.sentiment.polarity == 0:
-            return 0
-        else:
-            return -1
     
 
 def findusertweets(user_name):
 
     api = TwitterClient().get_twitter_client_api()
-    tweets = api.user_timeline(screen_name=user_name, count=10, exclude_replies=False, include_retweets=True)
+    tweets = api.user_timeline(screen_name=user_name, count=30, exclude_replies=False, include_retweets=True)
     tweet_analyzer = TweetAnalyzer()
     df = tweet_analyzer.tweets_to_data_frame(tweets)
-    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
     tweets_list = df['tweets']
     summarized_tweet = '. '.join(tweets_list)
-    client = ExpertAiClient()
-    document = client.specific_resource_analysis(
-    body={"document": {"text": summarized_tweet}}, 
-    params={'language': 'en', 'resource': 'sentiment'})
-    print("sentiment:", document.sentiment.overall)
     return summarized_tweet
 
 def pie_chart(taxonomy,summarized_tweet):
     client = ExpertAiClient()
     language= 'en'
     output = client.classification(body={"document": {"text": summarized_tweet}}, params={'taxonomy': taxonomy, 'language': language})
-    print("Tab separated list of categories:")
     pie_values =  {}
     for category in output.categories:
       pie_values[category.hierarchy[-1]] = category.frequency
-    print(pie_values)
     labels = list(pie_values.keys())
     values = list(pie_values.values())
     pull_values = [0 for i in values]
-    pull_values[values.index(max(values))] = 0.2
+    if len(values) != 0:
+        pull_values[values.index(max(values))] = 0.2
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label',hole=0.45,pull = pull_values, title  = taxonomy.upper())])
     fig.update_layout(width=650, height=650)
     div = to_html(fig)
-    return div
+    if len(labels) == 0:
+        status = 'ERROR'
+    else:
+        status = 'SUCCESS'
+    return div, status
--- twitter/views.py
+++ twitter/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import redirect, render
 from twitter.utils.twitter_sentiment_analysis import findusertweets, pie_chart
+import tweepy
 
 def analysis(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         if username != '':
-            summary = findusertweets(user_name=username)
-            bt_div = pie_chart('behavioral-traits', summary)
-            em_div = pie_chart('emotional-traits', summary)
-            return render(request, 'twitter/tweet-results.html', {'bt_div':bt_div, 'em_div':em_div})
+            try:
+                summary = findusertweets(user_name=username)
+                bt_div, bt_status = pie_chart('behavioral-traits', summary)
+                em_div, em_status = pie_chart('emotional-traits', summary)
+                if bt_status == 'ERROR' or em_status == 'ERROR':
+                    return render(request, 'twitter/tweet-results.html', {'error': 'Sorry :( We don\'t have enough information to run the prediction for this username'})
+                else:
+                    return render(request, 'twitter/tweet-results.html', {'bt_div':bt_div, 'em_div':em_div})
+            except tweepy.TweepError as e:
+                return render(request, 'twitter/tweet-results.html', {'error': 'Sorry :( We don\'t have enough information to run the prediction for this username'})        
+                
     return render(request, 'twitter/tweet-analysis.html')
 
 def tweet_results(request):
